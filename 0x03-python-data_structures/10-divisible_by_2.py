fff68d9fc7eb4519e13fecf290ff6e9431e8dee4
Ruba Salih <rubasalih900@gmail.com>
2024-01-08 14:20:46 +0300

    alx

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for i in range(len(my_list)):
+        j = my_list[i]
+        if j % 2 == 0:
+            my_list[i] = True
+        else:
+            my_list[i] = False
+    return (my_list)
