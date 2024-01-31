5a5e95da451e2fe80cd706bb887862950545ea38
Ruba Salih <rubasalih900@gmail.com>
2024-01-31 17:15:20 +0300

    alx

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,20 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    """
-    Function to generate a string "BestSchool" n times, where n is the number of the iteration.
-
-    Returns:
-    - str:
-        The generated string.
-
-    Example:
-    - If the iteration number is 3, the function will return "BestSchoolBestSchoolBestSchool".
-    """
-
-    # Getting the number of the iteration
-    n = 10  # Replace with your desired number of iterations
-
-    # Generating the string by repeating "BestSchool" n times
-    result = "BestSchool" * n
-
-    return result
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("Holberton, " * (magic_string.n - 1) + "Holberton")
