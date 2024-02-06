759105d92ff64ad5820958905710c324c4ab2998
Ruba Salih <rubasalih900@gmail.com>
2024-02-06 18:11:35 +0300

    als

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
+
 
 def read_file(filename=""):
+    """read_file function."""
+
+
     with open(filename, encoding='UTF-8') as f:
         print(f.read(), end= '')
