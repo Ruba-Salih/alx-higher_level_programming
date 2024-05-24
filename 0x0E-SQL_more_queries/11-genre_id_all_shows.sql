a93bc68692bc1582b601e2773e196c36c3d90aec
Ruba-Salih <rubasalih900@gmail.com>
2024-05-24 05:25:17 +0300

    10

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows JOIN tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC AND tv_show_genres.genre_id;
