f328b0f99855af9d183f26d504d35f9d13f96138
Ruba-Salih <rubasalih900@gmail.com>
2024-05-24 04:12:50 +0300

    0, 1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists all the tables of a database
-SHOW GRANTS FOR 'user_0d_1'@'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,2 @@
+-- lists all the tables of a database
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
