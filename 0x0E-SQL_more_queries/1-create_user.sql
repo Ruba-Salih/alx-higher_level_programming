7dd35e4a69f70264edd05a103aab8d6b28293ba2
Ruba-Salih <rubasalih900@gmail.com>
2024-05-24 04:16:47 +0300

    1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- lists all the tables of a database
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGE ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGE ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
