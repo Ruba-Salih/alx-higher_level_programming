26f55a55d1bd6f80d3f39c98dece23731186bfb4
Ruba-Salih <rubasalih900@gmail.com>
2024-05-24 04:53:34 +0300

    6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- lists all the tables of a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states' (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY NOT NULL , name VARCHAR(256) NOT NULL);
