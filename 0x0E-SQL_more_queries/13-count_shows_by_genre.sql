4ec5a040a1c79cf180c544ae63a3517f709cc607
Ruba-Salih <rubasalih900@gmail.com>
2024-05-24 05:39:54 +0300

    aaaaa

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
 -- Creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
-SELECT tv_genres.name AS genre, tv_genres.id AS number_of_shows
-FROM tv_genres JOIN tv_show_genres
+SELECT tv_genres.name AS genre, COUNT(tv_genres.id) AS number_of_shows
+FROM tv_genres INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY genre , number_of_shows DESC;
+ORDER BY genre
+ORDER BY number_of_shows DESC;
