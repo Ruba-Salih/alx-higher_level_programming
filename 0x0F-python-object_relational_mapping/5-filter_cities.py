2f8f8c69bf71ec102bec735c32c7b7ded0fe781e
Ruba-Salih <rubasalih900@gmail.com>
2024-06-24 01:37:10 +0300

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+"""
 
 import MySQLdb
 from sys import argv
 
 
 if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT cities.name \
+    cur.execute("SELECT cities.id, cities.name \
                  FROM cities JOIN states \
                  ON cities.state_id = states.id \
-                 WHERE states.name LIKE BINARY %s \
+                 WHERE states.name = %s \
                  ORDER BY cities.id ASC", (argv[4], ))
 
     for row in cur.fetchall():
