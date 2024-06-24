70ff91d61cf57bbbc40603c18440bef731c8e9dd
Ruba-Salih <rubasalih900@gmail.com>
2024-06-24 03:38:01 +0300

    task 9, 12 ,13 ,14, 100, 101, 102

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
+""" prints the State object with the name passed as argument from the database
 """
 import sys
 from model_state import Base, State
@@ -11,7 +11,10 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
+
     for instance in session.query(State).filter(State.name.like('%a%')):
-        print(instance.id, instance.name, sep=": ")
+        session.delete(instance)
+    session.commit()
